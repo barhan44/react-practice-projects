429f15762007d2c1b5fb199f3f81cbb7d40d0fbb
barhan44 <gunyashov44@gmail.com>
2020-01-26 23:05:51 +0300

    Finish...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './IPAddressContainer';
+import IPAddressContainer from "./IPAddressContainer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const destination = document.querySelector('#container');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <div>
+        <IPAddressContainer />
+    </div>,
+    destination
+);
