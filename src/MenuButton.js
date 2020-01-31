d56ff26ec535f2aeb610471ca43cb791fed9269e
barhan44 <gunyashov44@gmail.com>
2020-01-31 23:13:40 +0300

    Little refactoring

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -1,7 +1,8 @@
-import React, {Component} from "react";
+import React, {PureComponent} from "react";
 import './MenuButton.css';
 
-class MenuButton extends Component {
+class MenuButton extends PureComponent {
+
     render() {
         return (
             <button id="roundButton"
